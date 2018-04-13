42a4a79b4aaea8ed37b4842d6d36592a85417bac
blainmorin <blainmorin@brown.edu>
2018-04-12 20:59:50 -0400

    still troubleshooting

--- blainhw4.R
+++ blainhw4.R
@@ -16,79 +16,6 @@ authors <- function() {
 #### Question 1 ################################
 ####################################
 
-soft_thresholding <- function(x,a){
-  ## This could be done more efficiently using vector multiplication
-  ## See the forumula in slides
-  ##  sign(x)*pmax(abs(x) - a, 0)
-  result <- numeric(length(x))
-  result[which(x > a)] <- x[which(x > a)] - a
-  result[which(x < -a)] <- x[which(x < -a)] + a
-  return(result)
-}
-
-
-
-lasso_kkt_check <- function(X,y,beta,lambda, tol=1e-3){
-  ## check convergence 
-  beta <- as.matrix(beta); X <- as.matrix(X)
-  ## Assuming no intercepts 
-  G <- t(X)%*%(y-X%*%beta)/length(y)
-  ix <- which(beta == 0 )
-  iy <- which(beta != 0)
-  if (any(abs(G[ix]) > (lambda + tol) )) { return(pass=0) }
-  if (any(abs( G[iy] - lambda*sign(beta[iy] ) ) > tol)) { return(pass=0) }  
-  return(pass=1)
-}
-
-
-
-lasso.cd <- function(X,y,beta,lambda,tol=1e-6,maxiter=1000,quiet=FALSE){
-  # note that the LS part  in this function is the one in slides divided by length(y) = n 
-  ## Or equivalently  lambda here = n * lambda in class
-  beta <- as.matrix(beta); X <- as.matrix(X)
-  obj <- numeric(length=(maxiter+1))
-  betalist <- list(length=(maxiter+1))
-  betalist[[1]] <- beta
-  
-  for (j in 1:maxiter){
-    for (k in 1:length(beta)){
-      r <- y - X[,-k]%*%beta[-k]
-      beta[k] <- (1/norm(as.matrix(X[,k]),"F")^2)*soft_thresholding(t(r)%*%X[,k],length(y)*lambda)
-    }
-    betalist[[(j+1)]] <- beta
-    obj[j] <- (1/2)*(1/length(y))*norm(y - X%*%beta,"F")^2 + lambda*sum(abs(beta))
-    if (norm(betalist[[j]] - beta,"F") < tol) { break }
-  } 
-  check <- lasso_kkt_check(X,y,beta,lambda) 
-  
-  if (quiet==FALSE){
-    if (check==1) {
-      cat(noquote("Minimum obtained.\n"))
-    }
-    else { cat(noquote("Minimum not obtained.\n")) } 
-  }
-  return(list(obj=obj[1:j],beta=beta)) 
-}
-
-
-nothing_but_net = function(x, y, lambda, alpha) {
-  
-  xstandard = scale(x)
-  ystandard = scale(y)
-  lamb1 = lambda * alpha
-  lamb2 = .5*(1-alpha)*lambda
-  xstar = rbind(xstandard, lamb2 * diag(nrow = ncol(x)))
-  ystar = rbind(ystandard, as.matrix(rep(0, ncol(x))))
-  betas = lasso.cd(xstar, ystar, beta = rep(0, ncol(x)), lambda = lambda)
-  netbeta = ((1 + lamb2)^.5) * betas$beta
-  return(netbeta)
-  
-}
-
-test0 = nothing_but_net(x, y, 10, .95)
-test1 = glmnet(as.matrix(x), as.matrix(y), lambda = 10, alpha = .95)
-re = data.frame(me = test0, theirs = as.vector(test1$beta))
-View(re)
 
 ### Import data
 
@@ -96,3 +23,101 @@ x = read_csv("X.csv", col_names = FALSE)
 
 y = read_csv("y.csv", col_names = FALSE)
 
+
+## Using admm function from class
+
+softThresh <- function(x, lambda) {
+  sign(x)*pmax(0, abs(x) - lambda)
+}
+
+
+admmLasso <- function(X, y, tau, maxit = 1000, tol=1e-4) {
+  XX <- t(X) %*% X
+  Xy <- t(X) %*% y
+  
+  p <- ncol(X)
+  lambda <- rep(0, p)
+  maxRho <- 5
+  rho <- 4
+  
+  z0 <- z <- beta0 <- beta <- rep(0, p)
+  Sinv <- solve(XX + rho*diag(rep(1, p)) )
+  
+  for (it in 1:maxit) {
+    ## update beta
+    ## beta <- solve(XX + rho*diag(rep(1, p)) ) %*% (Xy + rho * z - lambda)
+    beta <- Sinv %*% (Xy + rho * z - lambda)
+    
+    ## update z
+    z <- softThresh(beta + lambda/rho, tau/rho)
+    ## update lambda
+    lambda <- lambda + rho* (beta - z ) 
+    ## increase rho
+    ## rho <- min(maxRho, rho*1.1)
+    
+    change <- max(  c( base::norm(beta - beta0, "F"),
+                       base::norm(z - z0, "F") ) )
+    if (change < tol || it > maxit) {
+      break
+    }
+    beta0 <-  beta
+    z0 <-  z
+    
+  }
+  z
+}
+
+
+## Using the solution from Zou and Hastie Paper
+## Put lambda1 and lambda2 in terms of the lamba and alpha formula from class
+## Basically, the function transforms the x and y matrix and then solves them like lasso
+
+
+## This one centers y and standardizes x
+nothing_but_net = function(x, y, lambda, alpha) {
+  
+  xstandard = scale(x)
+  ystandard = scale(y, scale = FALSE)
+  lamb1 = lambda * alpha
+  lamb2 = .5*(1-alpha)*lambda
+  xstar = ((1 +lamb2)**-.5) * rbind(xstandard, (lamb2^.5) * diag(nrow = ncol(x)))
+  ystar = rbind(ystandard, as.matrix(rep(0, ncol(x))))
+  betastar = admmLasso(xstar, ystar, tau = nrow(x))
+  netbeta = ((1 + lamb2)^.5) * betastar
+  return(netbeta)
+  
+}
+
+
+## This one does not scal x or y
+nothing_but_net2 = function(x, y, lambda, alpha) {
+  
+  xstandard = as.matrix(x)
+  ystandard = as.matrix(y)
+  lamb1 = lambda * alpha
+  lamb2 = .5*(1-alpha)*lambda
+  xstar = ((1 +lamb2)**-.5) * rbind(xstandard, (lamb2^.5) * diag(nrow = ncol(x)))
+  ystar = rbind(ystandard, as.matrix(rep(0, ncol(x))))
+  betastar = admmLasso(xstar, ystar, tau = nrow(x))
+  netbeta = ((1 + lamb2)^.5) * betastar
+  return(netbeta)
+  
+}
+
+## Run net with lambda = .01, .1, 1, and 10
+
+b1 = NULL
+
+lambdas = c(.01, .1, 1, 10)
+
+for(i in 1:length(lambdas)){
+  
+  b1 = cbind(b1, nothing_but_net2(x, y, lambdas[i], alpha = .95))
+  
+}
+
+for(i in 1:length(lambdas)){
+  fit = glmnet(as.matrix(x), as.matrix(y), lambda = lambdas[i], alpha = .95, standardize = FALSE, intercept = FALSE)
+  b1 = cbind(b1, as.numeric( coef(fit) )[-1])
+  
+}
